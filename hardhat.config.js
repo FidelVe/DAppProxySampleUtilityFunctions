ae0f653ca8f4cd35105bf5ee30fe2cbb05294bd4
FidelVe <43641727+FidelVe@users.noreply.github.com>
2023-05-10 18:56:42 -0400

    refactoring

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,17 +1,17 @@
 require("dotenv").config();
 require("@nomiclabs/hardhat-ethers");
 
-const PK1 = process.env.PK1 == null ? "" : process.env.PK1;
+// const PK1 = process.env.PK1 == null ? "" : process.env.PK1;
 
 const config = {
-  networks: {
-    hardhat2: {
-      url: "https://server03.espanicon.team",
-      chainId: 1337,
-      gasPrice: 20000000000,
-      accounts: [PK1],
-    },
-  },
+  // networks: {
+  //   hardhat2: {
+  //     url: "https://server03.espanicon.team",
+  //     chainId: 1337,
+  //     gasPrice: 20000000000,
+  //     accounts: [PK1],
+  //   },
+  // },
   paths: {
     sources: "./solidity/contracts",
     tests: "./solidity/test",
